f816098264c76c4fabd562ae744503cf4f3a3b04
PajarKharisma <pajarkharisma@hotmail.com>
2019-05-19 16:43:14 +0700

    add bst array

---
+++ 2 - BST/BstArray.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+typedef int Tree[500000];
+int maxIndex = 0;
+
+void insertData(Tree tree, int data, int index){
+    if(tree[index] == 0){
+        tree[index] = data;
+        if(index > maxIndex)
+            maxIndex = index;
+    }else{
+        int newIndex = (data < tree[index]) ? (2*index)+1 : (2*index)+2;
+        insertData(tree, data, newIndex);
+    }
+}
+
+void preOrder(Tree tree, int index){
+    if(tree[index] != 0){
+        cout << tree[index] << " ";
+        preOrder(tree, (2*index)+1);
+        preOrder(tree, (2*index)+2);
+    }
+}
+
+void inOrder(Tree tree, int index){
+    if(tree[index] != 0){
+        preOrder(tree, (2*index)+1);
+        cout << tree[index] << " ";
+        preOrder(tree, (2*index)+2);
+    }
+}
+
+void postOrder(Tree tree, int index){
+    if(tree[index] != 0){
+        preOrder(tree, (2*index)+1);
+        preOrder(tree, (2*index)+2);
+        cout << tree[index] << " ";
+    }
+}
+
+void levelOrder(Tree tree){
+    for(int i=0; i<=maxIndex; i++){
+        if(tree[i] != 0){
+            cout << tree[i] <<  " ";
+        }
+    }
+}
+
+int findMax(Tree tree){
+    int index = 0;
+    while(tree[(2*index)+2] != 0){
+        index = (2*index)+2;
+    }
+    return tree[index];
+}
+
+int main(){
+    Tree tree = {0};
+    int arr[] = {25, 73, 41, 30, 58, 64, 98, 13, 87, 91, 17, 76, 28, 45, 56};
+	int n = sizeof(arr)/sizeof(arr[0]);
+
+    for(int i=0; i<n; i++){
+        insertData(tree, arr[i], 0);
+    }
+    cout << "Maximum element is : " << findMax(tree);
+    //levelOrder(tree);
+}
--- 3 - Hashing/tempCodeRunnerFile.cpp
+++
@@ -1,9 +0,0 @@
-                int i = 1;
-                int newPos = index;
-                while(database[newPos] != 0){
-                    newPos = index;
-                    int quadratic = pow(i, 2);
-                    newPos = (index + quadratic) % NUM_OF_INDEX;
-                    i++;
-                }
-                database[newPos] = data;
