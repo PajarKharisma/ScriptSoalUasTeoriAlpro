06fb8ee2a13c1b4c1ae500f9b99b168ccf7ba7d6
PajarKharisma <pajarkharisma@hotmail.com>
2019-05-20 15:54:21 +0700

    add compare counter

--- 8 - Sorting/Main.cpp
+++ 8 - Sorting/Main.cpp
@@ -185,6 +185,7 @@ class SelectionSort{
             for(int i=0; i<n; i++){
                 int minIndex = i;
                 for(int j=i+1; j<n; j++){
+                    counter++;
                     if(data[j] < data[minIndex]){
                         minIndex = j;
                     }
